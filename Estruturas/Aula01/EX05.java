d41b3083ab360c12b1a8474245ade4ac0bd67043
Gustavo <ggarabetti0607@gmail.com>
2023-08-18 01:13:52 -0300

    final-version

--- Estruturas/Aula01/EX05.java
+++ Estruturas/Aula01/EX05.java
@@ -12,25 +12,31 @@ public class EX05{
 		int qtd[] = new int[LIM_SUP];
 		boolean vis[] = new boolean[LIM_SUP];
 
-		for(int i = 0; i < N; i++) qtd[i] = 0;
-
 		for(int i = 0; i < N; i++){
 			System.out.print("v[" + i + "]: ");
 			vetor[i] = entrada.nextInt();
 			qtd[vetor[i]]++;
 		}
 
+		int vetor_resp[] = new int[2*LIM_SUP];
+
 		System.out.print("Vetor resposta: ");
+
+		int j = 0;
 
 		for(int i = 0; i < N; i++){
 			if(!vis[vetor[i]]){
-				System.out.print(vetor[i] + " " + qtd[vetor[i]] + " ");
+				vetor_resp[j] = vetor[i];
+				j++;
+				vetor_resp[j] = qtd[vetor[i]];
+				j++;
 				vis[vetor[i]] = true;
 			}
 		}
 
-		System.out.println();
+		for(int i = 0; i < j; i++) System.out.printf("%d ", vetor_resp[i]);
 
+		System.out.println();
 		entrada.close();
 	}
 }
